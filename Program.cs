d4c532063cd0a090434762b007cf2bd82da9e011
Lemuel Bonite <ljbonite21@gmail.com>
2020-10-30 16:41:26 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Neo4jClient;
+using System;
+
+namespace Neo4jLab
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var client = new GraphClient(new Uri("http://localhost:7474/db/products", "neo4j", "Spidey21"));
+        }
+
+        public static int RequestInt(string message)
+        {
+            System.Console.Write(message);
+            int input = int.Parse(System.Console.ReadLine());
+            System.Console.WriteLine();
+            return input;
+        }
+
+        public static string RequestString(string message)
+        {
+            System.Console.Write(message);
+            string input = System.Console.ReadLine();
+            System.Console.WriteLine();
+
+            return input;
+        }
+    }
+}
